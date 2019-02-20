03ba54d4557939f84c3de475ead6783e1f756935
t.nakamura <t.nakamura@toreta.in>
2019-02-20 14:56:57 +0900

    rxswift implementation

--- CountdownTimer/CounterViewModel.swift
+++ CountdownTimer/CounterViewModel.swift
@@ -6,22 +6,96 @@
 //  Copyright © 2019 t.nakamura. All rights reserved.
 //
 
-class CounterViewModel {
-    private(set) var count = 0
-    
-    func incrementCount(callback: (Int) -> ()) {
-        count += 1
-        callback(count)
-    }
-    
-    func decrementCount(callback: (Int) -> ()) {
-        count -= 1
-        callback(count)
-    }
-    
-    func resetCount(callback: (Int) -> ()) {
-        count = 0
-        callback(count)
-    }
-    
+import RxSwift
+import RxCocoa
+
+struct CounterViewModelInput {
+    let countUpButton: Observable<Void>
+    let countDownButton: Observable<Void>
+    let countResetButton: Observable<Void>
 }
+
+protocol CounterViewModelOutput {
+    var counterText: Driver<String?> { get }
+}
+
+protocol CounterViewModelType {
+    var outputs: CounterViewModelOutput? { get }
+    func setup(input: CounterViewModelInput)
+}
+
+class CounterViewModel: CounterViewModelType {
+    var outputs: CounterViewModelOutput?
+    
+    private let countRelay = BehaviorRelay<Int>(value: 0)
+    private let initialCount = 0
+    private let disposeBag = DisposeBag()
+    
+    init() {
+        self.outputs = self
+        resetCount()
+    }
+    
+    func setup(input: CounterViewModelInput) {
+        input.countUpButton
+            .subscribe(onNext: { [weak self] in
+                self?.incrementCount()
+            })
+            .disposed(by: disposeBag)
+        
+        input.countDownButton
+            .subscribe(onNext: { [weak self] in
+                self?.decrementCount()
+            })
+            .disposed(by: disposeBag)
+        
+        input.countResetButton
+            .subscribe(onNext: { [weak self] in
+                self?.resetCount()
+            })
+            .disposed(by: disposeBag)
+        
+    }
+    
+    private func incrementCount() {
+        let count = countRelay.value + 1
+        countRelay.accept(count)
+    }
+    
+    private func decrementCount() {
+        let count = countRelay.value - 1
+        countRelay.accept(count)
+    }
+    
+    private func resetCount() {
+        countRelay.accept(initialCount)
+    }
+}
+
+extension CounterViewModel: CounterViewModelOutput {
+    var counterText: Driver<String?> {
+        return countRelay
+            .map { "Rx パターン:\($0)" }
+            .asDriver(onErrorJustReturn: nil)
+    }
+}
+
+//class CounterViewModel {
+//    private(set) var count = 0
+//
+//    func incrementCount(callback: (Int) -> ()) {
+//        count += 1
+//        callback(count)
+//    }
+//
+//    func decrementCount(callback: (Int) -> ()) {
+//        count -= 1
+//        callback(count)
+//    }
+//
+//    func resetCount(callback: (Int) -> ()) {
+//        count = 0
+//        callback(count)
+//    }
+//
+//}
--- CountdownTimer/ViewController.swift
+++ CountdownTimer/ViewController.swift
@@ -6,40 +6,47 @@
 //  Copyright © 2019 t.nakamura. All rights reserved.
 //
 
-import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     
     
     @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var countUpButton: UIButton!
+    @IBOutlet weak var countDownButton: UIButton!
+    @IBOutlet weak var countResetButton: UIButton!
+    
+    private let disposeBag = DisposeBag()
     
     private var viewModel: CounterViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViewModel()
+    }
+    
+    private func setupViewModel() {
         viewModel = CounterViewModel()
+        let input = CounterViewModelInput(
+            countUpButton: countUpButton.rx.tap.asObservable(),
+            countDownButton: countDownButton.rx.tap.asObservable(),
+            countResetButton: countResetButton.rx.tap.asObservable()
+        )
+        viewModel.setup(input: input)
+        
+        viewModel.outputs?.counterText
+            .drive(countLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        
     }
     
     
-    @IBAction func countUp(_ sender: Any) {
-        viewModel.incrementCount(callback: { [weak self] count in
-            self?.updateCountLabel(count)
-        })
-    }
+
     
     
-    @IBAction func countDown(_ sender: Any) {
-        viewModel.decrementCount(callback: { [weak self] count in
-            self?.updateCountLabel(count)
-        })
-    }
-    
-    
-    @IBAction func countReset(_ sender: Any) {
-        viewModel.resetCount(callback: { [weak self] count in
-            self?.updateCountLabel(count)
-        })
-    }
+
         
     private func updateCountLabel(_ count: Int){
         countLabel.text = String(count)
